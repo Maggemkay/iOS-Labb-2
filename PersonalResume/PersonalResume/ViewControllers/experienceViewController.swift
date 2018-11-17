8ac8556880574da617805766a114eced3c376ca5
Marcus Gullstrand <guma1502@mace2428-13.jth-stud1.hj.se>
2018-11-17 17:35:03 +0100

    Not done. Trying to fix custom classes.

--- PersonalResume/PersonalResume/ViewControllers/experienceViewController.swift
+++ PersonalResume/PersonalResume/ViewControllers/experienceViewController.swift
@@ -1,20 +1,22 @@
 //
-//  ViewController.swift
+//  experienceViewController.swift
 //  PersonalResume
 //
 //  Created by Marcus Gullstrand on 2018-11-17.
 //  Copyright © 2018 Marcus Gullstrand. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class experienceViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    
+    
+    
 }
-
---
+++ PersonalResume/PersonalResume/ViewControllers/mainViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PersonalResume
+//
+//  Created by Marcus Gullstrand on 2018-11-17.
+//  Copyright © 2018 Marcus Gullstrand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+//    @IBAction func experienceButton(_ sender: Any) {
+//        performSegue(withIdentifier: "mainToExperience", sender: self)
+//    }
+    
+    
+    
+
+}
+

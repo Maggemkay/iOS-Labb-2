823b84cc3760096b1a325a8461b32665517466fe
Marcus Gullstrand <guma1502@mace2428-13.jth-stud1.hj.se>
2018-11-17 17:55:54 +0100

    Try fix...

--- PersonalResume/PersonalResume/ViewControllers/experienceViewController.swift
+++ PersonalResume/PersonalResume/ViewControllers/experienceViewController.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2018 Marcus Gullstrand. All rights reserved.
 //
 
-import Foundation
+//import Foundation
 import UIKit
 
 class experienceViewController: UIViewController {
---
+++ PersonalResume/PersonalResume/ViewControllers/testViewController.swift
@@ -0,0 +1,13 @@
+//
+//  testViewController.swift
+//  PersonalResume
+//
+//  Created by Marcus Gullstrand on 2018-11-17.
+//  Copyright © 2018 Marcus Gullstrand. All rights reserved.
+//
+
+import UIKit
+
+class testViewController: NSObject {
+
+}
